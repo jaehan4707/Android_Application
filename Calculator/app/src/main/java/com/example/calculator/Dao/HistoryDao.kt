96646677648918761c539e505339ea91ae392cd9
Leejaehan <jahan4707@naver.com>
2022-09-07 16:59:38 +0900

    DB 파일(Dao, 데이터항목, 데이터베이스 클래스 )생성

---
+++ Calculator/app/src/main/java/com/example/calculator/Dao/HistoryDao.kt
@@ -0,0 +1,29 @@
+package com.example.calculator.Dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.calculator.model.History
+
+// room에 연결된 Dao
+// entitiy 조회등 어떻게 할건지
+@Dao
+interface HistoryDao {
+
+    @Query("SELECT * FROM history_table") // 쿼리문 작성
+    fun getAll(): List<History>
+
+    @Insert
+    fun insertHistory(history: History)
+
+    @Query("DELETE FROM history_table") // 테이블 전체 삭제
+    fun deleteAll()
+
+    @Delete // 해당 히스트로리만 제거
+    fun delete(history: History)
+
+    // 조건 가진 결과만 가져오기 (1개만)
+    @Query("SELECT * FROM history_table WHERE result LIKE :result LIMIT 1")
+    fun findByResult(result: String): History
+}
